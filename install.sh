6b874941d44ed82023fcf74fb2f96e4e83b2abd6
Michael Crockett <mcrockett86@icloud.com>
2025-09-03 23:04:28 -0700

    wip install base repo with additional deps

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 # !/bin/bash
 
+######### Client Side Application ###########
+
 # install the dependencies
 npm install
 
@@ -12,5 +14,28 @@ npm run predeploy
 # push to the gh-pages branch
 npm run deploy
 
-# deployment                                                                                      │
+# deployment
 https://create-react-app.dev/docs/deployment/#github-pages
+
+
+
+######### Server Side Application ###########
+
+git clone git@github.com:pixegami/simple-rag-pipeline.git src_pydantic_ai
+
+# remove the .git directory in the submodule repository
+git rm --cached src_pydantic_ai -f 
+rm -rf "src_pydantic_ai/.git"
+rm "src_pydantic_ai/.gitignore"
+
+python3 -m venv src_pydantic_ai/venv
+source src_pydantic_ai/venv/bin/activate
+python3 -m pip install -r src_pydantic_ai/requirements.txt
+
+# add pydantic-ai repo
+python3 -m pip install pydantic-ai
+
+# TODO: need to package into requirements.txt used for the RAG repo for deployment later
+python3 -m pip freeze >> src_pydantic_ai/requirements_frozen.txt
+
+##############################################
